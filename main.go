7abea287ddef66b8a0f99baf4f4109f1d8ac3a19
Sandertv <st.ten.veldhuis@gmail.com>
2017-11-07 21:04:35 +0100

    Go version checking, implemented fairly stable tick speed adapting.

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"gomine"
 	time2 "time"
+	"runtime"
 )
 
 var currentTick int = 0
 
 func main() {
+	if !checkRequirements() {
+		return
+	}
 
-	var server = gomine.NewServer()
+	var server, error = gomine.NewServer()
+	if error != nil {
+		fmt.Println("Another instance of the server is already running.")
+		return
+	}
 	server.Start()
-	var tickDuration = 50 * int64(time2.Millisecond)
+
+	var tickDrop = 20
 
 	for {
+		var tickDuration = int(1.0 / float32(server.GetTickRate()) * 1000) * int(time2.Millisecond)
 		var nextTime = time2.Now().Add(time2.Duration(tickDuration))
 
 		server.Tick(currentTick)
@@ -22,9 +32,21 @@ func main() {
 		var diff = nextTime.Sub(time2.Now()).Nanoseconds()
 
 		if diff > 0 {
+			tickDrop--
+
+			if tickDrop < 0 && server.GetTickRate() != 20 && diff > 5 * int64(time2.Millisecond) {
+				server.SetTickRate(server.GetTickRate() + 1)
+				fmt.Println("Elevating tick rate to:", server.GetTickRate())
+			}
+
 			time2.Sleep(time2.Duration(diff))
 		} else {
-			// Tick time took longer than it should. Lower tick rate?
+			tickDrop++
+
+			if tickDrop > 40 {
+				server.SetTickRate(server.GetTickRate() - 1)
+				fmt.Println("Lowering tick rate to:", server.GetTickRate())
+			}
 		}
 
 		if !server.IsRunning() {
@@ -37,4 +59,14 @@ func main() {
 	}
 
 	// Other shutdown code.
+}
+
+func checkRequirements() bool {
+	var version = runtime.Version()
+	if version != "go1.9.2" {
+		fmt.Println("Please install the GoLang 1.9.2 release.")
+		return false
+	}
+
+	return true
 }
--- src/gomine/Server.go
+++ src/gomine/Server.go
@@ -1,27 +1,67 @@
 package gomine
 
+import "errors"
+
 type server struct {
 	isRunning bool
+	tickRate int
 }
 
-func NewServer() server {
-	return server{}
+var started bool = false
+
+func NewServer() (server, error) {
+	var errorServer server
+	if started {
+		return errorServer, errors.New("cannot create a second server")
+	}
+
+	var server = server{}
+	server.tickRate = 20
+	started = true
+	return server, nil
 }
 
 func (server *server) IsRunning() bool {
 	return server.isRunning
 }
 
+/*
+ *
+ */
 func (server *server) Start() {
 	server.isRunning = true
 }
 
+/*
+ * Shuts down the server.
+ */
 func (server *server) Shutdown() {
 	server.isRunning = false
 }
 
 /*
- * Internal. Do not use manually.
+ * Returns the tick rate of the server.
+ */
+func (server *server) GetTickRate() int {
+	return server.tickRate
+}
+
+/*
+ * Resets the tick value back to the default. (20)
+ */
+func (server *server) ResetTickRate() {
+	server.tickRate = 20
+}
+
+/*
+ * Internal. Not to be used by plugins.
+ */
+func (server *server) SetTickRate(tickRate int) {
+	server.tickRate = tickRate
+}
+
+/*
+ * Internal. Not to be used by plugins.
  */
 func (server *server) Tick(currentTick int) {
 
