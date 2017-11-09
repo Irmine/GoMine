2ed06f8ec832c5e1abde29a2754653f3795d569b
Angel Maya <xbeastmode00@gmail.com>
2017-11-08 23:55:04 -0500

    fix some binary issues

--- src/gomine/utils/Binary.go
+++ src/gomine/utils/Binary.go
@@ -2,7 +2,28 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 )
+
+func get(buffer *[]byte, offset *int, length int) (byte, error) {
+
+	if (len(*buffer)-1) == *offset {
+		return 0, errors.New("no bytes left to read")
+	}
+
+	if length > 1 {
+		var out byte
+		for i := 0; i < length; i++ {
+			out += (*buffer)[*offset]
+			*offset++
+		}
+		return out, nil
+	}
+
+	out := (*buffer)[*offset]
+	*offset++
+	return out, nil
+}
 
 func WriteBool(buffer *[]byte, bool bool) {
 	if bool {
@@ -12,32 +33,35 @@ func WriteBool(buffer *[]byte, bool bool) {
 	*buffer = append(*buffer, 0x00)
 }
 
-func ReadBool(buffer *[]byte, offset *int) (bool, error) {
-	if len(*buffer) == 0 {
-		return false, errors.New("no bytes left to read")
+func ReadBool(buffer *[]byte, offset *int, length int) (bool) {
+	out, err := get(buffer, offset, length)
+	if err != nil {
+		fmt.Printf("An error occurred: %v", err)
+		panic("Aborting...")
 	}
-	*offset++
-	return (*buffer)[*offset] != 0x00, nil
+	return out != 0x00
 }
 
-func ReadByte(buffer *[]byte, offset *int) (byte, error) {
-	if len(*buffer) == 0 {
-		return 0, errors.New("no bytes left to read")
+func ReadByte(buffer *[]byte, offset *int, length int) (byte) {
+	out, err := get(buffer, offset, length)
+	if err != nil {
+		fmt.Printf("An error occurred: %v", err)
+		panic("Aborting...")
 	}
-	*offset++
-	return byte((*buffer)[*offset]), nil
+	return byte(out)
 }
 
 func WriteByte(buffer *[]byte, byte byte) {
 	*buffer = append(*buffer, byte)
 }
 
-func ReadInt(buffer *[]byte, offset *int) (int, error) {
-	if len(*buffer) == 0 {
-		return 0, errors.New("no bytes left to read")
+func ReadInt(buffer *[]byte, offset *int, length int) (int) {
+	out, err := get(buffer, offset, length)
+	if err != nil {
+		fmt.Printf("An error occurred: %v", err)
+		panic("Aborting...")
 	}
-	*offset++
-	return int((*buffer)[*offset]), nil
+	return int(out)
 }
 
 func WriteInt(buffer *[]byte, int int) {
